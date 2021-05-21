467e2d3af5a32636b6c231940c441a369efaa99a
Kyle Salmon <kyleraysalmon@gmail.com>
2021-05-21 15:46:44 -0400

    Updated ESLINT Config

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+// ESLINT: https://eslint.org/docs/rules/
+// TSLINT: https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin
+
+// Add Extends
+// 'plugin:import/errors'
+// 'plugin:react/recommended'
+
+// Add/Remove Rule
+// "@typescript-eslint/explicit-module-boundary-types": "error"
+
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:cypress/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "eslint-config-prettier"
+  ],
+  plugins: [
+    "react-hooks",
+    "@emotion/eslint-plugin",
+    "@typescript-eslint/eslint-plugin"
+  ],
+  rules: {
+    "strict": ["error", "never"],
+    "react-hooks/rules-of-hooks": "warn",
+    "react-hooks/exhaustive-deps": "warn",
+    "no-case-declarations": "warn",
+    "no-else-return": "error",
+    "no-multi-spaces": "error",
+    "eol-last": 2,
+    "eqeqeq": 2,
+    "no-fallthrough": [
+      "warn",
+      {
+        "commentPattern": "falls through"
+      }
+    ],
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/consistent-type-definitions": ["warn", "interface"],
+    "@typescript-eslint/explicit-module-boundary-types": "off"
+  },
+};
